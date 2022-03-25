11f780adb70bec5fb25b942f68df37a353e4fa4d
nguyendanhtienanh <nguyendanhtienanh@gmail.com>
2022-03-25 15:33:10 +0700

    trang login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Login from './Components/Login';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <Login></Login>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <Login></Login>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,22 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
-import Logo from '../Img/logo.jpg';
+import logo from '../img/logo.jpg';
+import {Input} from 'reactstrap';
+import imguser from '../img/logo-user.png';
+import imgpassword from '../img/logo-password.png';
+import imginvisible from '../img/logo-invisible.png';
+import {Button} from 'reactstrap';
 
 class Login extends Component {
     render() {
-        let url = Logo;
-        return (
-            <div className='login-background'>
-                <div className='login-container'>
-                    <div className='login-logo'>
-                        <img src={url}/>
+        return(
+            <div className="Login">
+                <div className="background">
+                    <div className="logo">
+                        <img src = {logo}/>
                     </div>
-                    <div className='login-content'>
-                        
+                    <div className="username">
+                        <div>
+                            <Input
+                                placeholder="Tên đăng nhập"
+                            />
+                            <img src={imguser}></img>
+                        </div>
+                    </div>
+                    <div className="password">
+                        <div className="pass-logo">
+                            <Input
+                            type="password"
+                            placeholder="Mật khẩu"
+                            />
+                            <img src={imgpassword}></img>
+                        </div>
+                        <img src={imginvisible}></img>
+                    </div>
+                    <div className="remember">
+                            <Input
+                                type="checkbox"
+                                valid
+                            />
+                            <p>Lưu đăng nhập</p>
+                    </div>
+                    <div className="forget">
+                        <p>Bạn quên mật khẩu?</p>
+                    </div>
+                    <div className="log">
+                        <Button>
+                            Đăng nhập
+                        </Button>
+                    </div>
+                    <div className="info">
+                        <p>Nếu bạn có thắc mắc hay cần giải đáp, vui lòng liên hệ số điện thoại: 19001000</p>
+                    </div>
+                    <div className="license">
+                        <p>Bản quyền thuộc về AnyBim</p>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
